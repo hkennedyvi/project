7aaaedbca98facf6644e11ed73ad080a3c3c9065
Howard Kennedy <hkennedyvi@gmail.com>
2020-11-04 12:55:53 -0800

    main page styling

--- client/src/components/Test.vue
+++ client/src/components/Test.vue
@@ -1,27 +1,60 @@
 <template>
-  <div>TEST</div>
+  <div class="main-container">
+    <div class="left">1</div>
+    <div class="float-box">
+      <div class="pic-box">
+
+      </div>
+      <h1 class="header">we're glad you're here.</h1>
+    </div>
+    <div class="right"></div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Test'
-}
+  name: "Test",
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+.main-container {
+  margin: 0;
   padding: 0;
+  display: flex;
+  height: 100vh;
+  background: lightslategrey;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.left {
+  background: white;
+  flex: 1;
+  position: relative;
 }
-a {
-  color: #42b983;
+
+.float-box {
+  height: 60%;
+  width: 70%;
+  border: solid 1px black;
+  position: absolute;
+  left: 20%;
+  top: 20%;
+  display: flex;
+}
+
+.pic-box {
+  border: solid 1px black;
+  width: 50%;
+}
+
+.header {
+  align-self: flex-end;
+  margin: 0 0 0 auto;
+}
+
+.right {
+  background: orange;
+  flex: 2;
 }
 </style>
