cccc5af69231b85e454465da09e0852454fd04fe
Howard Kennedy <hkennedyvi@gmail.com>
2020-11-07 15:57:41 -0800

    bottom animation, slide in

--- client/src/components/Bottom.vue
+++ client/src/components/Bottom.vue
@@ -1,8 +1,35 @@
 <template>
   <div class="bottom-container">
     BOTTOM
-    <div v-bind:class="[inView ? 'test-color' : '', 'box']" v-on:click="test">
-  </div>
+    <button v-on:click="animateBox">CLICK</button>
+    <div class="box-container">
+      <div v-bind:class="[inView ? 'box-second' : 'box-first']" v-on:click="test">
+        <h1>OUR FOUNDER</h1>
+        <img class="box-image" src="../assets/defender.jpg" alt="">
+      </div>
+      <div v-bind:class="[inView ? 'box-fourth' : 'box-third']" v-on:click="test">
+        <p class="box-copy">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+          anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+          anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+          anim id est laborum.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,8 +47,12 @@ export default {
       if (position === 1794) {
         this.inView = !this.inView;
       } else {
-      console.log(position);
+        console.log(position);
       }
+    },
+    animateBox() {
+      this.inView = !this.inView;
+      console.log("hello");
     },
   },
 };
@@ -30,15 +61,56 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .bottom-container {
-  background: white;
+  background: rgb(32, 32, 32);
+  color: white;
   height: 45rem;
 }
 
-.box {
+.box-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.box-image {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.box-copy {
+  line-height: 2rem;
+  padding: 2rem;
+}
+
+.box-first {
   height: 40rem;
   width: 40rem;
-  border: solid 1px black;
-  margin-left: 30%;
+  border: solid 1px white;
+  margin-left: 0;
+  transition: 700ms ease-in;
+}
+
+.box-second {
+  height: 40rem;
+  width: 40rem;
+  border: solid 1px white;
+  margin-left: 10%;
+  transition: 700ms ease-in;
+}
+
+.box-third {
+  height: 40rem;
+  width: 40rem;
+  border: solid 1px white;
+  margin-right: 0;
+  transition: 700ms ease-in;
+}
+
+.box-fourth {
+  height: 40rem;
+  width: 40rem;
+  border: solid 1px white;
+  margin-right: 10%;
+  transition: 700ms ease-in;
 }
 
 .test-color {
