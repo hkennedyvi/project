2988eb9b03322e44945e69375d1c78cc37924ff1
Howard Kennedy <hkennedyvi@gmail.com>
2020-11-24 14:27:57 -0800

    heroku debug

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV !== 'production') {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/public/'));
 
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+    app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 };
 
 const PORT = process.env.PORT || 5000;
